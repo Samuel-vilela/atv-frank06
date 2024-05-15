4e09ba0d6fc1b2c40d354f81e72d743af059ce8c
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-05-15 19:33:45 -0300

    Adicionar arquivos de projeto.

---
+++ atv-frank06/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace atv_frank06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Click
+            // 
+            this.btn_Click.BackColor = System.Drawing.Color.IndianRed;
+            this.btn_Click.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Click.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_Click.FlatAppearance.BorderSize = 9;
+            this.btn_Click.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_Click.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Click.Font = new System.Drawing.Font("AcadEref", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Click.ForeColor = System.Drawing.Color.OliveDrab;
+            this.btn_Click.Location = new System.Drawing.Point(306, 188);
+            this.btn_Click.Name = "btn_Click";
+            this.btn_Click.Size = new System.Drawing.Size(201, 74);
+            this.btn_Click.TabIndex = 0;
+            this.btn_Click.Text = "Click Aqui ";
+            this.btn_Click.UseVisualStyleBackColor = false;
+            this.btn_Click.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Click;
+    }
+}
+
